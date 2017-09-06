33d56f9df3dbb19a3ca7927ec2f7ce73e92b30d3
dalekRider <jmercier76@gmail.com>
2017-09-05 21:29:07 -0400

    Uploaded Files
    Uploaded files the old fashioned way. Atom Editor appears to be having issues and the Magic 8-Ball has advised against using it again this evening.

---
+++ magicEightBallv2.js
@@ -0,0 +1,41 @@
+
+const magicEightBallSays = function() {
+
+let userName = prompt('What is your Name?');
+userName ? window.alert('Hello, ' + userName + '. Think of a question and then ask') : window.alert('Hello, your identity will remain a secret. Think of a question and then ask');
+let userQuestion = prompt('What is your question...');
+let randomNumber = Math.floor(Math.random() * 7)
+let eightBall = '';
+
+switch(randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Don\'t count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+window.alert(userName + ' has asked: ' + userQuestion);
+window.alert('The Magic 8Ball says: ' + eightBall +'.')
+};
+
+magicEightBallSays();
